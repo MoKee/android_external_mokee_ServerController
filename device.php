c6baf3aac598145d8c40f2b8351bdafa1b000b41
martincz <martincz@mokeedev.com>
2015-09-18 15:31:36 +0800

    add G620-A2 to device list
    Change-Id: I5d4bb148fcfbc5f0f75ff1d6546bc2736916e6eb

--- device.php
+++ device.php
@@ -45,6 +45,7 @@ $device_fullname = array(
     'find7s' => 'Oppo Find 7 (S)',
     'flo' => 'Google Nexus 7 2013 (Wi-Fi)',
     'flounder' => 'Google Nexus 9 (Wi-Fi)',
+    'g620_a2' => 'Huawei G620-A2',
     'galaxysmtd' => 'Samsung Galaxy S',
     'ghost' => 'Motorola Moto X (Unified)',
     'grouper' => 'Google Nexus 7 (Wi-Fi)',
