44b8136d659fbbe691d130797ad881c5bb2c7083
martincz <martincz@mokeedev.com>
2016-08-23 22:17:56 +0800

    add z2pro maintainer info
    Change-Id: If838ace79b230d887f6e6acbcddc0753f9a19332

--- maintainers.php
+++ maintainers.php
@@ -2,4 +2,6 @@
 $maintainers_info = array(
     'oneplus3' => array('name' => '电撸鸡逃',
                      'url' => 'http://weibo.com/dianlujitao'),
+    'z2pro' => array('name' => 'Flour_Mo',
+                     'url' => 'http://weibo.com/2036597861'),
 );
