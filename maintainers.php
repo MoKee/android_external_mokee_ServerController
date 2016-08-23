dfcae51c1eb9dbd5f6ec236ffb1f11213e0e7356
martincz <martincz@mokeedev.com>
2016-08-23 22:30:12 +0800

    update some maintainer info
    Change-Id: I8e5a732612b85a968ec5cb5899f391918156ca13

--- maintainers.php
+++ maintainers.php
@@ -1,5 +1,13 @@
 <?php
 $maintainers_info = array(
+    'cp8675' => array('name' => 'Flour_Mo',
+                     'url' => 'http://weibo.com/2036597861'),
+    'kenzo' => array('name' => 'Flour_Mo',
+                     'url' => 'http://weibo.com/2036597861'),
+    'libra' => array('name' => 'xiaobai不会写shell',
+                     'url' => 'http://weibo.com/3138080394'),
+    'nx531j' => array('name' => 'XiNGRZ',
+                     'url' => 'http://weibo.com/1260797924'),
     'oneplus3' => array('name' => '电撸鸡逃',
                      'url' => 'http://weibo.com/dianlujitao'),
     'z2pro' => array('name' => 'Flour_Mo',
