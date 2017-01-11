548b89ac2e969632d6229e346be3cc5279f0c9cb
Jesse Chan <cjx123@outlook.com>
2017-01-11 12:46:15 +0000

    add infos of Galaxy S7 Flat and Edge
    Change-Id: I49803d117ab75a7aa91754e4f7e02406cfab4c49

--- recovery.php
+++ recovery.php
@@ -1,5 +1,7 @@
 <?php
 $recovery_info = array(
+    'herolte' => 'http://teamw.in/devices/samsunggalaxys7.html',
+    'hero2lte' => 'http://teamw.in/devices/samsunggalaxys7edge.html',
     'nx523j' => 'http://bbs.nubia.cn/thread-680629-1-1.html',
     'nx531j' => 'https://www.androidfilehost.com/?w=files&flid=125093',
 );
