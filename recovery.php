b512912b24d09983c65b9ac136b70a895dac3cf9
dianlujitao <dianlujitao@gmail.com>
2017-01-12 14:56:00 +0800

    Add correct TWRP download link for cherry
    Change-Id: I8f6a7a4c10fa602f15c6592bbe44c8b3b952c14f

--- recovery.php
+++ recovery.php
@@ -1,5 +1,7 @@
 <?php
 $recovery_info = array(
+    'che10' => 'https://www.androidfilehost.com/?fid=673368273298920883',
+    'cherry' => 'https://www.androidfilehost.com/?fid=673368273298920883',
     'herolte' => 'http://teamw.in/devices/samsunggalaxys7.html',
     'hero2lte' => 'http://teamw.in/devices/samsunggalaxys7edge.html',
     'nx523j' => 'http://bbs.nubia.cn/thread-680629-1-1.html',
