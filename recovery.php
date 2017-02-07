073ee9f75972b338042891dde5ec9deeebd027d6
chenzc <dazzworld@163.com>
2017-02-08 01:36:59 +0800

    nx529j:update recovery download link
    Change-Id: Ic2474e1331a333cd5e2c06771b0ef952e085a1e9

--- recovery.php
+++ recovery.php
@@ -8,6 +8,7 @@ $recovery_info = array(
     'hero2lte' => 'http://teamw.in/devices/samsunggalaxys7edge.html',
     'k3gxx' => 'https://www.androidfilehost.com/?fid=24459283995296044',
     'nx523j' => 'http://bbs.nubia.cn/thread-680629-1-1.html',
+    'nx529j' => 'https://pan.baidu.com/s/1kVqe77h',
     'nx531j' => 'https://www.androidfilehost.com/?w=files&flid=125093',
     'oneplus3' => 'https://www.androidfilehost.com/?fid=745425885120700618',
     'sumire' => 'https://pan.baidu.com/s/1dEDaIuH',
