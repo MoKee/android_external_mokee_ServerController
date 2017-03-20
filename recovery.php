85de148707b89c0d75bd9dafa41d3fb23c25aef6
GuaiYiHu <guaiyihu@foxmail.com>
2017-03-20 10:12:56 +0800

    leo: add leo recovery download link.
    Change-Id: I2f39b9a348af02c413540ac6d82921f1edc93af3

--- recovery.php
+++ recovery.php
@@ -7,6 +7,7 @@ $recovery_info = array(
     'herolte' => 'http://teamw.in/devices/samsunggalaxys7.html',
     'hero2lte' => 'http://teamw.in/devices/samsunggalaxys7edge.html',
     'k3gxx' => 'https://www.androidfilehost.com/?fid=24459283995296044',
+    'leo' => 'https://pan.baidu.com/s/1eSDXDVc',
     'nx523j' => 'http://bbs.nubia.cn/thread-680629-1-1.html',
     'nx529j' => 'https://pan.baidu.com/s/1kVqe77h',
     'nx531j' => 'https://www.androidfilehost.com/?w=files&flid=125093',
