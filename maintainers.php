1e2039cc612cdd67b5cc6c615f7062e50972dde7
zhaochengw <zhaochengw@163.com>
2017-03-21 17:49:15 +0800

    add lenovo x3
    Change-Id: I96a26b130b315df6050ec7da88fb74e962c27ff4

--- maintainers.php
+++ maintainers.php
@@ -50,6 +50,8 @@ $maintainers_info = array(
                      'url' => 'http://weibo.com/parheliamm'),
     'sumire' => array('name' => 'parhelia',
                      'url' => 'http://weibo.com/parheliamm'),
+    'x3' => array('name' => 'zhaochengw',
+                     'url' => 'http://weibo.com/1649884475'),
     'z2' => array('name' => 'Flour_Mo',
                      'url' => 'http://weibo.com/2036597861'),
     'z2pro' => array('name' => 'Flour_Mo',
