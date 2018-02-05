6632aa7671d2aff6a88bb6f301fe31c0e7666abc
martincz <martincz@mokeedev.com>
2018-02-06 05:37:49 +0800

    fix filesize unit
    Change-Id: I2978027bb6f9d84edf7fa572d6283ff0b292e904

--- extras.php
+++ extras.php
@@ -4,7 +4,7 @@ $extras_info = array(
         'radio' => array(
             array(
                 'filename' => 'RADIO-dumpling-20180129203617.zip',
-                'filesize' => '59.8 MB',
+                'filesize' => '62737479',
                 'summary' => 'Since MK81.0 20180201',
                 'timestamp' => '1517229377',
                 'url' => 'https://github.com/xingrz/proprietary_vendor_oneplus_dumpling_radio/releases/tag/oxygen%2Fob-3',
