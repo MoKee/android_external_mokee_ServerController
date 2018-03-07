491b3646ea3bb04b92ed5303abf8055b93308649
XiNGRZ <chenxingyu92@gmail.com>
2018-03-07 12:57:07 +0800

    extras: Add radio urls for cheeseburger, update timestamps
    Change-Id: I61e8f2b0b5d5ea4b8e009f5384584ef496dfc08f

--- extras.php
+++ extras.php
@@ -1,21 +1,46 @@
 <?php
 $extras_info = array(
+    'cheeseburger' => array(
+        'radio' => array(
+            array(
+                'filename' => 'RADIO-cheeseburger-20171205221614.zip',
+                'filesize' => '58680989',
+                'summary' => 'MK71.2 20171216',
+                'timestamp' => '1513382400',
+                'url' => 'https://androidfilehost.com/?fid=746010030569959670',
+            ),
+            array(
+                'filename' => 'RADIO-cheeseburger-20180129201642.zip',
+                'filesize' => '62735063',
+                'summary' => 'MK81.0 20180201',
+                'timestamp' => '1517443200',
+                'url' => 'https://androidfilehost.com/?fid=890129502657588259',
+            ),
+            array(
+                'filename' => 'RADIO-cheeseburger-20180223011306.zip',
+                'filesize' => '62735641',
+                'summary' => 'MK81.0 20180307',
+                'timestamp' => '1520380800',
+                'url' => 'https://androidfilehost.com/?fid=962187416754465697',
+            ),
+        ),
+    ),
     'dumpling' => array(
         'radio' => array(
             array(
                 'filename' => 'RADIO-dumpling-20180129203617.zip',
                 'filesize' => '62737479',
                 'summary' => 'MK81.0 20180201',
-                'timestamp' => '1517229377',
+                'timestamp' => '1517443200',
                 'url' => 'https://androidfilehost.com/?fid=890129502657588262',
             ),
-            // Multi radio demo
-            // array(
-            //     'filename' => 'RADIO-dumpling-20181234567890.zip',
-            //     'filesize' => '61.4 MB',
-            //     'summary' => 'MK81.0 20180215',
-            //     'url' => 'https://xxxxxxxxxxx',
-            // ),
+            array(
+                'filename' => 'RADIO-dumpling-20180223013435.zip',
+                'filesize' => '62730345',
+                'summary' => 'MK81.0 20180307',
+                'timestamp' => '1520380800',
+                'url' => 'https://androidfilehost.com/?fid=673956719939825035',
+            ),
         ),
     ),
 );
