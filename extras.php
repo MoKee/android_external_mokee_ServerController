9ace74a36f8723ed9c867ffb4988f9553b9d9288
XiNGRZ <chenxingyu92@gmail.com>
2018-04-05 21:53:31 +0800

    extras: oneplus5: Remove outdated radio
    Change-Id: I1649685fc749233fd94c9dc0ea91b14a24f64db1

--- extras.php
+++ extras.php
@@ -10,20 +10,6 @@ $extras_info = array(
                 'url' => 'https://androidfilehost.com/?fid=746010030569959670',
             ),
             array(
-                'filename' => 'RADIO-cheeseburger-20180129201642.zip',
-                'filesize' => '62735063',
-                'summary' => 'MK81.0 20180201',
-                'timestamp' => '1517414400',
-                'url' => 'https://androidfilehost.com/?fid=890129502657588259',
-            ),
-            array(
-                'filename' => 'RADIO-cheeseburger-20180223011306.zip',
-                'filesize' => '62735641',
-                'summary' => 'MK81.0 20180307',
-                'timestamp' => '1520352000',
-                'url' => 'https://androidfilehost.com/?fid=962187416754465697',
-            ),
-            array(
                 'filename' => 'RADIO-cheeseburger-20180309182027.zip',
                 'filesize' => '63649624',
                 'summary' => 'MK81.0 20180318',
@@ -34,20 +20,6 @@ $extras_info = array(
     ),
     'dumpling' => array(
         'radio' => array(
-            array(
-                'filename' => 'RADIO-dumpling-20180129203617.zip',
-                'filesize' => '62737479',
-                'summary' => 'MK81.0 20180201',
-                'timestamp' => '1517414400',
-                'url' => 'https://androidfilehost.com/?fid=890129502657588262',
-            ),
-            array(
-                'filename' => 'RADIO-dumpling-20180223013435.zip',
-                'filesize' => '62730345',
-                'summary' => 'MK81.0 20180307',
-                'timestamp' => '1520352000',
-                'url' => 'https://androidfilehost.com/?fid=673956719939825035',
-            ),
             array(
                 'filename' => 'RADIO-dumpling-20180310222638.zip',
                 'filesize' => '63650062',
