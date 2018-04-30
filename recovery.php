00679eb04c8d92cf2912851a246bde3e2f992d5b
GaHoKwan <kwangaho@gmail.com>
2018-04-30 20:04:29 +0800

    extra: add jsglte recovery
    Change-Id: I263a9a90e739287ebdb45dd78fe4b0254025f759

--- recovery.php
+++ recovery.php
@@ -13,6 +13,7 @@ $recovery_info = array(
     'j5ltexx' => 'https://www.androidfilehost.com/?w=files&flid=56318',
     'j5nltexx' => 'https://www.androidfilehost.com/?w=files&flid=56315',
     'jason' => 'https://androidfilehost.com/?fid=889764386195931802',
+    'jsglte' => 'https://github.com/GaHoKwan/twrp_device_samsung_jsglte/raw/android-7.1/prebuilt/twrp-3.2.1-0-jsglte.img.tar',
     'leo' => 'https://pan.baidu.com/s/1i5RM4Ix',
     'nx523j' => 'http://bbs.nubia.cn/thread-680629-1-1.html',
     'nx529j' => 'https://pan.baidu.com/s/1kVqe77h',
