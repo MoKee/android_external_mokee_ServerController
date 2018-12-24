1075bed08bbce5b7dd820e12b5d34f5d50326681
XiNGRZ <chenxingyu92@gmail.com>
2018-12-24 12:37:40 +0800

    extras: Hide colombo & surabaya until ready
    Change-Id: I0072219d2821eb57a5a5fbbeaeff5ad12acd4135

--- extras.php
+++ extras.php
@@ -18,17 +18,17 @@ $extras_info = array(
             )
         )
     ),
-    'colombo' => array(
-        'radio' => array(
-            array(
-                'filename' => 'RADIO-colombo-20181016155943.zip',
-                'filesize' => '48850128',
-                'summary' => 'MK71.2 20181204',
-                'timestamp' => '1543852800',
-                'url' => 'https://androidfilehost.com/?fid=11410963190603865847'
-            )
-        )
-    ),
+    // 'colombo' => array(
+    //     'radio' => array(
+    //         array(
+    //             'filename' => 'RADIO-colombo-20181016155943.zip',
+    //             'filesize' => '48850128',
+    //             'summary' => 'MK71.2 20181204',
+    //             'timestamp' => '1543852800',
+    //             'url' => 'https://androidfilehost.com/?fid=11410963190603865847'
+    //         )
+    //     )
+    // ),
     'dumpling' => array(
         'radio' => array(
             array(
@@ -116,17 +116,17 @@ $extras_info = array(
             )
         )
     ),
-    'surabaya' => array(
-        'radio' => array(
-            array(
-                'filename' => 'RADIO-surabaya-20181016154239.zip',
-                'filesize' => '48856459',
-                'summary' => 'MK71.2 20181204',
-                'timestamp' => '1543852800',
-                'url' => 'https://androidfilehost.com/?fid=11410963190603866033'
-            )
-        )
-    ),
+    // 'surabaya' => array(
+    //     'radio' => array(
+    //         array(
+    //             'filename' => 'RADIO-surabaya-20181016154239.zip',
+    //             'filesize' => '48856459',
+    //             'summary' => 'MK71.2 20181204',
+    //             'timestamp' => '1543852800',
+    //             'url' => 'https://androidfilehost.com/?fid=11410963190603866033'
+    //         )
+    //     )
+    // ),
     'whyred' => array(
         'radio' => array(
             array(
