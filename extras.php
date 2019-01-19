83c58527712c3bde20d8a4dd0c9598c1de1d8e33
Martincz Gao <martincz@mokeedev.com>
2019-01-20 05:44:01 +0800

    Derp
    Change-Id: Ie3f38fc1ddf19f9c17d89b75009e9532beb7597a

--- extras.php
+++ extras.php
@@ -3,37 +3,8 @@ $extras_info = array(
     'cancro' => array(
         'recovery' => 'https://pan.baidu.com/s/1aIRJoqeIxr5IGU06y9ZBUQ'
     ),
-    'cheeseburger' => array(
-        'radio' => array(
-            array(
-                'filename' => 'RADIO-cheeseburger-20171205221614.zip',
-                'filesize' => '58680989',
-                'summary' => 'MK71.2 20171216',
-                'timestamp' => '1513353600',
-                'url' => 'https://androidfilehost.com/?fid=746010030569959670'
-            ),
-            array(
-                'filename' => 'RADIO-cheeseburger-20181010200421.zip',
-                'filesize' => '63840028',
-                'summary' => 'MK81.0 20181217',
-                'timestamp' => '1544806700',
-                'url' => 'https://androidfilehost.com/?fid=11410963190603875622'
-            )
-        )
-    ),
     'clover' => array(
         'recovery' => 'https://pan.baidu.com/s/1HgjIWw2WPdnNEdpMN2GCTw'
-    ),
-    'dumpling' => array(
-        'radio' => array(
-            array(
-                'filename' => 'RADIO-dumpling-20181025005754.zip',
-                'filesize' => '63840634',
-                'summary' => 'MK81.0 20181217',
-                'timestamp' => '1544806700',
-                'url' => 'https://androidfilehost.com/?fid=11410963190603875626'
-            )
-        )
     ),
     'leo' => array(
         'recovery' => 'https://pan.baidu.com/s/1bmFbrLFj8rH9FLn3V-rt8A'
