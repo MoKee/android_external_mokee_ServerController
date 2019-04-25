3367edf44ed2cf9321faefd7d84dad512381c5d5
subdragonzj <zj502407679@gmail.com>
2019-04-26 00:24:20 +0800

    extras: Add radio for lavender
    Change-Id: Ie377b82a90b222277e5f652a54ca3172087c68a7

--- extras.php
+++ extras.php
@@ -12,9 +12,6 @@ $extras_info = [
     'icesky' => [
         'recovery' => 'https://bbs.mokeedev.com/t/topic/11917'
     ],
-    'lavender' => [
-        'recovery' => 'https://www.androidfilehost.com/?fid=1395089523397949015'
-    ],
     'leo' => [
         'recovery' => 'https://www.androidfilehost.com/?w=files&flid=197979'
     ],
@@ -23,6 +20,18 @@ $extras_info = [
     ],
     'nx531j' => [
         'recovery' => 'https://share.weiyun.com/5EvT4FK'
+    ],
+    'lavender' => [
+        'radio' => [
+            [
+                'filename' => 'RADIO_lavender_V10.3.1.0.PFGCNXM.zip',
+                'filesize' => '63317625',
+                'summary' => 'MK90.0 20190426',
+                'timestamp' => '1556207192',
+                'url' => 'https://www.androidfilehost.com/?fid=1395089523397951766'
+            ]
+        ],
+        'recovery' => 'https://www.androidfilehost.com/?fid=1395089523397949015'
     ],
     'nx563j' => [
         'radio' => [
