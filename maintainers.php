71e4560845cac249c4e4d8a3ea3b0c58e8caa11e
Martincz Gao <martincz@mokeedev.com>
2019-04-30 10:21:13 +0800

    Derp
    Change-Id: Ice4a2359c484412c8a238e29a847b9e894e3a4f0

--- maintainers.php
+++ maintainers.php
@@ -1,27 +1,28 @@
 <?php
 $maintainers_info = [
     'martincz' => [
-        'title' => 'Founder & Lead Developer',
-        'bio' => '¯\_(ツ)_/¯',
-        'github' => 'martincz',    // https://github.com/?
+        'title'     => 'Founder & Lead Developer',
+        'bio'       => '¯\_(ツ)_/¯',
+        'github'    => 'martincz', // https://github.com/?
         'community' => 'martincz', // https://bbs.mokeedev.com/u/?
-        'telegram' => 'martincz',   // https://t.me/?
-        'weibo' => 'martincz'      // http://weibo.com/?
+        'telegram'  => 'martincz', // https://t.me/?
+        'weibo'     => 'martincz', // http://weibo.com/?
+        'xda'       => '5172180'   // https://forum.xda-developers.com/member.php?u=?
     ],
     'XiNGRZ' => [
-        'title' => 'Developer & Device Maintainer',
-        'bio' => '[点击编辑个性签名]',
-        'github' => 'xingrz',
+        'title'     => 'Developer & Device Maintainer',
+        'bio'       => '[点击编辑个性签名]',
+        'github'    => 'xingrz',
         'community' => 'xingrz',
-        'telegram' => 'smartisandev',
-        'devices' => ['icesky', 'ocean', 'osborn', 'oscar', 'oxford', 'trident']
+        'telegram'  => 'smartisandev',
+        'devices'   => ['icesky', 'ocean', 'osborn', 'oscar', 'oxford', 'trident']
     ],
     'GuaiYiHu' => [
-        'title' => 'Device Maintainer',
-        'bio' => '我是乖亦虎，我木有小丁丁',
-        'github' => 'guaiyihu',
+        'title'     => 'Device Maintainer',
+        'bio'       => '我是乖亦虎，我木有小丁丁',
+        'github'    => 'guaiyihu',
         'community' => 'guaiyihu',
-        'telegram' => 'guaiyihu',
-        'devices' => ['whyred', 'virgo']
+        'telegram'  => 'guaiyihu',
+        'devices'   => ['whyred', 'virgo']
     ],
 ];
